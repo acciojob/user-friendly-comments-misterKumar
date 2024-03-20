5f0bfa538d779e5f57a3ef3c5f6db886dea4ea8f
misterKumar <120304503+misterKumar@users.noreply.github.com>
2024-03-20 19:56:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
 //your JS code here. If required.
-const commentsContianer=document.querySelector(".comments-container");
-const textBoxInput=document.querySelector("#textInput");
-const submitBtn=document.querySelector("#submit-btn");
+const commentsContainer = document.querySelector(".comments-container");
+const textBoxInput = document.querySelector("#textInput");
+const submitBtn = document.querySelector("#submit-btn");
 
-submitBtn.addEventListener("click",()=>{
-	if(textBoxInput.value.length!==0){
-		const div=document.createElement("div");
-		div.classList.add("comment")
-		const date=new Date().toUTCString();
-		div.innerHTML=`
-						<h1>${textBoxInput.value}</h1>
-							<div class="author">
-								<p>Mister Kumar</p>
-								<p>${date}</p>
-							</div>
-  `
-		commentsContianer.appendChild(div);
-	}
-})
+submitBtn.addEventListener("click", () => {
+    if (textBoxInput.value.length !== 0) {
+        const div = document.createElement("div");
+        div.classList.add("comment");
+
+        const date = new Date(); // Current date and time
+        const dateString = date.toUTCString(); // Convert date to UTC string
+
+        div.innerHTML = `
+            <h1>${textBoxInput.value}</h1>
+            <div class="author">
+                <p>Mister Kumar</p>
+                <p>${dateString}</p>
+            </div>
+        `;
+
+        commentsContainer.prepend(div); // Prepend new comment to display in descending order
+        textBoxInput.value = ""; // Clear input field after submission
+    }
+});
