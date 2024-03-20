b9d4e543328c6f7c28ec94970691ee70898da983
misterKumar <120304503+misterKumar@users.noreply.github.com>
2024-03-20 19:32:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+const commentsContianer=document.querySelector(".comments-container");
+const textBoxInput=document.querySelector("#textInput");
+const submitBtn=document.querySelector("#submit-btn");
+
+submitBtn.addEventListener("click",()=>{
+	if(textBoxInput.value.length!==0){
+		const div=document.createElement("div");
+		div.classList.add("comment")
+		const date=new Date().toUTCString();
+		div.innerHTML=`
+						<h1>${textBoxInput.value}</h1>
+							<div class="author">
+								<p>Mister Kumar</p>
+								<p>${date}</p>
+							</div>
+  `
+		commentsContianer.appendChild(div);
+	}
+})
